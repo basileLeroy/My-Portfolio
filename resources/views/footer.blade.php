e708f53a9e836f9c4ef817974fbc82197705e04e
Basile Leroy <basile.leroy.jnm@hotmail.com>
2021-02-09 11:34:45 +0100

    Using blade to extend general frames (header/footer/...) into a view page

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,10 @@
+@section('footer')
+    <footer class="bg-light text-center text-lg-start">
+        <!-- Copyright -->
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2)">
+            © 2021 Copyright:
+            <a class="text-dark" href="#">Basile&Co</a>
+        </div>
+        <!-- Copyright -->
+    </footer>
+@endsection
