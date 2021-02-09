ca189e273d0ea2edabefdf064fba8fd40d5a5b33
Basile Leroy <basile.leroy.jnm@hotmail.com>
2021-02-09 12:05:42 +0100

    Adding a form to the welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 @extends('footer')
 
 @section('intro')
-    <div class="welcome" style="color:honeydew; width: 40vw; height:77vh; padding-left:50px; margin-top: 50px">
+    <div class="welcome" style="color:honeydew; width: 40vw; padding-left:50px; margin-top: 50px">
         <h1>WELCOME!</h1>
         <br><br>
         <p>Welcome to my page! here you will find the home page, or welcome page where I used blades to import sections into specific pages of the webiste. like this one.</p>
@@ -12,7 +12,18 @@
 
 
 @section('content')
-
+    <form action="" method="POST">
+        <div class="form" style="display: flex; flex-direction:column; justify-content:center; width: 40%; margin: auto; margin-top: 100px; margin-bottom: 160px">
+            <h3 style="color: honeydew;">Do You like my page?</h3>
+            <select name="select" id="select" style="width:50%">
+                <option value="yes">YES</option>
+                <option value="no">NO</option>
+                <option value="undecided">UNDECIDED</option>
+            </select>
+            <br>
+            <button type="submit" name="submit" id="submit" style="width:50%">Submit</button>
+        </div>
+    </form>
 @endsection
 
 
