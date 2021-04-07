195be74c5e11a97a0f98a16e63a9333eb7859b5b
Basile Leroy <basile.leroy.jnm@hotmail.com>
2021-04-07 19:15:36 +0200

    Cleaning up revieuw page

--- resources/views/review.blade.php
+++ resources/views/review.blade.php
@@ -7,40 +7,32 @@
 @endsection
 
 
-@section('intro')
-<div class="welcome" style="color:honeydew; width: 40vw; padding-left:50px; padding-top: 20px">
-    <h1>HELLO WORLD!</h1>
-    <br>
-    <p>
-        Here are some of the fines reviews! <br>
-        
-    </p>
-</div> 
-@endsection
+
 
 
 @section('content')
 
-<div class="create-review">
-    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-    Add your review!
-    </button>
-</div>
+        
+    <div class="bg-light position-sticky start-0 translate-middle-y ml-3 p-4 rounded-3" style="width: 200px; top: 200px">
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+            Add your review!
+        </button>
+    </div>
 
     <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Your Review</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                <div class="modal-body">
-                    <form action="/review" method="POST" enctype="multipart/form-data">
+                <div class="modal-body" style="width: 80%; margin: auto;">
+                    <form action="/review" method="POST" enctype="multipart/form-data" style="width: 100%; align-items:center; margin: auto;">
                         {{ csrf_field() }}
-                        <div class="form" style="display: flex; flex-direction:column; text-align:center; align-items:center; justify-content:center; width: 40%; margin: auto; margin-top: 50px; margin-bottom: 50px">
-                            <h3 style="color: honeydew;">Do You like my page?</h3>
+                        <div class="form" style="display: flex; flex-direction:column; text-align:center; justify-content:center; margin: auto;">
+                            <h3 style="color: #6d6d6d;">Do You like my page?</h3>
                             <hr>
-                            <input style="width:50%; text-align:center" type="text" name="name" placeholder="Your name ...">
+                            <input style="text-align:center" type="text" name="name" placeholder="Your name ...">
                             @section('error')
                                 <div class="d-flex justify-content-center -w-25">
                                     @foreach ($errors->all() as $error )
@@ -49,26 +41,26 @@
                                 </div>
                             @endsection
                             <br>
-                            <select name="select" id="select" style="width:50%; text-align:center">
+                            <select name="select" id="select" style=" text-align:center">
                                 <option value="yes">YES</option>
                                 <option value="no">NO</option>
                             </select>
                             <br>
-                            <textarea id="subject" name="review" placeholder="Tell me WHYY.. Ain't nothing but a hea-..." style="width:50%;"></textarea>
+                            <textarea id="subject" name="review" placeholder="Tell me WHYY.. Ain't nothing but a hea-..." style=""></textarea>
                             <br>
-                            <button type="submit" name="submit" id="submit" value="ok" style="width:50%; text-align:center">Submit</button>
+                            <button type="submit" name="submit" id="submit" value="ok" style="text-align:center">Submit</button>
                         </div>
                     </form>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
+                    <button type="button" class="btn btn-primary">Save review</button>
                 </div>
             </div>
         </div>
     </div>
 
-    <div class="container review">
+    <div class="container review" style="margin-top: 30px;">
         @foreach ($posts as $post)
         <div class="card w-50 p-3 start-50 translate-middle">
             <div class="card-header">
