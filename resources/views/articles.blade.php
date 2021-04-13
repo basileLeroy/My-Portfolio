1177f97737a92f38ece01482ad4672796f7dc940
Basile Leroy <basile.leroy.jnm@hotmail.com>
2021-04-13 18:24:23 +0200

    Adding link to article

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -14,7 +14,7 @@
     <div class="card position-absolute top-50 start-50 translate-middle" style="width: 30rem;">
         <img class="card-img-top" src="{{url('/images/Choosing_framework.png')}}" alt="Card image cap">
         <div class="card-body">
-            <h3 class="card-title py-2"><a href="">Choosing a Framework in Node.js</a></h3>
+            <h3 class="card-title py-2"><a href="https://dev.to/basileleroy/choosing-a-framework-in-node-js-3n">Choosing a Framework in Node.js</a></h3>
             <p class="card-text py-3">
                 I can only assume that every person that started coding, has been confronted with this situation... <br>
                 Where you want to achieve a certain feature or use a certain structure to ease the process when working on a project. <br><br>
