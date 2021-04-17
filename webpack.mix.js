5a2c03d90edff3f964a472e25e16a5651e7ec484
Basile Leroy <basile.leroy.jnm@hotmail.com>
2021-04-17 19:55:47 +0200

    Removing tailwind from webpack for now

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,6 +15,3 @@ mix.js('resources/js/app.js', 'public/js')
     .sass('resources/css/app.scss', 'public/css', [
         //
     ])
-    .postCss("resources/css/app.css", "public/css", [
-        require("tailwindcss"),
-    ]);
