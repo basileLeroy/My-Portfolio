b485294b894f15691816fef2f11849242d6dcfad
Basile Leroy <basile.leroy.jnm@hotmail.com>
2021-04-21 18:21:44 +0200

    Fixing footer issues: reactivity and positioning

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -41,11 +41,10 @@
                 @yield('content')
                 @yield('error')
                 @yield('review')
-                @yield('footer')
             </div>
         @yield('rvwModal')
-
-
+        @yield('footer')
+        
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js" integrity="sha384-aJ21OjlMXNL5UyIl/XNwTMqvzeRMZH2w8c5cRVpzpU8Y5bApTppSuUkhZXN0VxHd" crossorigin="anonymous"></script>
     </body>
