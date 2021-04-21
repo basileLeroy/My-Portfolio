fc23b4c9da32a93771db48ce0fb9c438963638dc
Basile Leroy <basile.leroy.jnm@hotmail.com>
2021-04-21 12:46:45 +0200

    Fixing meta tags for facebook

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,8 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <meta property="og:image" content="{{ asset('images/meta_card.png') }}"/>
-        <!-- <meta property="og:image:width" content="1200" />
-        <meta property="og:image:height" content="630" /> -->
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
         <meta property="og:title" content="Basile&Code"/>
         <meta property="og:description" content="Hi, I am Basile. A junior full stack web dev. And this is my portfolio."/>
         <meta property="og:type" content="website"/>
