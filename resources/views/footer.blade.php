13df2e4e18f04ba5b64f890259390c068005428f
Basile Leroy <basile.leroy.jnm@hotmail.com>
2021-05-03 15:50:08 +0200

    Adding google analytics to project

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -6,5 +6,16 @@
             <a class="text-dark" href="/contact">Basile&Code</a>
         </div>
         <!-- Copyright -->
+
+        <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-185794040-2"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-185794040-2');
+        </script>
+
     </footer>
 @endsection
